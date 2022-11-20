ae9201a2d918cf8078a55f0330e48e69a5a9411c
MisterSlimak <93765820+MisterSlimak@users.noreply.github.com>
2022-11-20 20:41:13 +0100

    node update v1

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-// Import excel
+// Import dependencies
 const fs = require("fs");
 const XLSX = require("xlsx");
 
 // Read the file into memory
-const excel = XLSX.readFile("base.xlsx");
+// const workbook = XLSX.read(fs.readFileSync("file-example.xlsx"));
+const workbook = XLSX.readFile("base.xlsx");
 
-// Convert the excel to JSON
-let dataBase = {};
-
-// loop over every sheet
-for (const sheetName of excel.SheetNames) {
-
-    // XLSX.utils.sheet_to_json generates an array of objects
-    dataBase[sheetName] = XLSX.utils.sheet_to_json(excel.Sheets[sheetName]);
+// Convert the XLSX to JSON
+let worksheets = {};
+for (const sheetName of workbook.SheetNames) {
+    // Some helper functions in XLSX.utils generate different views of the sheets:
+    //     XLSX.utils.sheet_to_csv generates CSV
+    //     XLSX.utils.sheet_to_txt generates UTF16 Formatted Text
+    //     XLSX.utils.sheet_to_html generates HTML
+    //     XLSX.utils.sheet_to_json generates an array of objects
+    //     XLSX.utils.sheet_to_formulae generates a list of formulae
+    worksheets[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
 }
 
-// test
-console.log(dataBase)
+// Show the data as JSON
+console.log(worksheets);
